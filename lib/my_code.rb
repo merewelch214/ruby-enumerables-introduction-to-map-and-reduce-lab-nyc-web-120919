c94a2766ae4dc836f2329740a57ac3e444cfcff9
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-13 19:50:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,77 @@
-# My Code here....
+def map_to_negativize(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i] * -1) 
+    i += 1
+  end 
+  new_array
+end
+
+def map_to_no_change(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i]) 
+    i += 1
+  end 
+  new_array
+end
+
+def map_to_double(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i] * 2) 
+    i += 1
+  end 
+  new_array
+end
+
+def map_to_square(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i] ** 2) 
+    i += 1
+  end 
+  new_array
+end
+
+def reduce_to_total(array, starting_point = 0)
+  i = 0
+  total = starting_point
+  while i < array.length do
+    total += array[i] 
+    i += 1
+  end 
+  total
+end
+
+def reduce_to_all_true(array)
+  i = 0
+  true_arr = []
+  while i < array.length do
+    if array[i]
+      true_arr.push(array[i])
+    else 
+      return false
+    end
+    i += 1
+  end 
+  true_arr
+end
+
+def reduce_to_any_true(array)
+  i = 0
+  final = nil
+  while i < array.length do
+    if array[i]
+      final = true
+    else 
+      final = false
+    end
+    i += 1
+  end 
+  final
+end
