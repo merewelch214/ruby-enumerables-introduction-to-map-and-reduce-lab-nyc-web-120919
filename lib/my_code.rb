7065735fb62475edbe3a53dae572d45e016cc427
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-13 19:34:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,3 +37,13 @@ def map_to_square(array)
   end 
   new_array
 end
+
+def reduce_to_total(array)
+  i = 0
+  total = 0
+  while i < array.length do
+    total += array[i] 
+    i += 1
+  end 
+  total
+end
