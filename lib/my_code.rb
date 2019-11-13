fb1a8030e720f28fbaf6d45325b1f33a4f0be05a
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-13 19:33:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def map_to_square(array)
   i = 0
   new_array = []
   while i < array.length do
-    new_array.push(array[i] ^ 2) 
+    new_array.push(array[i] ** 2) 
     i += 1
   end 
   new_array
